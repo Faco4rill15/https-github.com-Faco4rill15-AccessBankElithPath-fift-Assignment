6ae1e2fa2da7145af22925cabe8c710f13d2583f
Faco4rill15 <148912713+Faco4rill15@users.noreply.github.com>
2024-05-03 12:35:39 -0700

    Add files via upload

---
+++ 05 Assignment 5/index.js
@@ -0,0 +1,19 @@
+const change = document.querySelector("h1")
+
+change.textContent = "Scholarship Journey "
+change.style.textAlign = "center"
+change.style.color = "red"
+change.style.fontFamily = "arial"
+
+
+const contain = document.getElementById("main")
+contain.innerHTML  = `<h3>David Sampson is a Lecturer in EliPath</h3>`
+contain.style.background = "gray"
+contain.style.color = "white"
+contain.fontFamily = "arial"
+contain.style.textAlign = "center"
+
+
+
+
+
